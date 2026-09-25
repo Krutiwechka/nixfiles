285435df278b0a8c6fa09108fccd192c61fe912f
Timojj <krutiwechka@gmail.com>
2026-09-25 14:59:55 +0300

    cliphist

---
+++ home-manager/modules/cliphist.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  home.packages = [ pkgs.cliphist pkgs.wl-clipboard ];
+
+  wayland.windowManager.hyprland.settings.exec-once = [
+    "wl-paste --watch cliphist store"
+  ];
+  wayland.windowManager.hyprland.settings.bind = [
+    "$mod, V, exec, ${pkgs.cliphist}/bin/cliphist list | ${pkgs.rofi}/bin/rofi -dmenu -p clipboard -theme-str 'window {width: 600px;}' | ${pkgs.cliphist}/bin/cliphist decode | ${pkgs.wl-clipboard}/bin/wl-copy"
+  ];
+}
+
